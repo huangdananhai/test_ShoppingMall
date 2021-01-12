99ab6ffaab14926afb49537abced7b6268a21325
huangdananhai <18310338011@163.com>
2021-01-12 17:52:32 +0800

    第三次提交

--- src/views/home/gerenxinxi.vue
+++ src/views/home/gerenxinxi.vue
@@ -6,24 +6,30 @@
     <el-card class="header_card" shadow="always">
       <img src="../../assets/img/header.jpg" alt />
       <div class="left">
-      <el-form
-        :label-position="labelPosition"
-        label-width="80px"
-        :model="formLabelAlign"
-      >
-        <el-form-item label="账户">
-          <el-input v-model="formLabelAlign.name"></el-input>
-        </el-form-item>
-        <el-form-item label="密码">
-          <el-input v-model="formLabelAlign.region"></el-input>
-        </el-form-item>
-        <el-form-item label="修改密码">
-          <el-input v-model="formLabelAlign.type"></el-input>
-        </el-form-item>
-      </el-form>
+        <el-form
+          :label-position="labelPosition"
+          label-width="80px"
+          :model="ruleForm"
+          ref="ruleForm"
+        >
+          <el-form-item label="账户" prop="name">
+            <el-input v-model="ruleForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="region">
+            <el-input type="password" v-model="ruleForm.region"></el-input>
+          </el-form-item>
+          <el-form-item label="修改密码" prop="type">
+            <el-input type="password" v-model="ruleForm.type"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')"
+              >提交</el-button
+            >
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
       </div>
     </el-card>
-
   </div>
 </template>
 
@@ -32,12 +38,23 @@ export default {
   data() {
     return {
       labelPosition: "left",
-      formLabelAlign: {
+      ruleForm: {
         name: "",
         region: "",
         type: "",
       },
     };
+  },
+  methods: {
+    submitForm(formName) {
+      console.log("用户"+this.ruleForm.name)
+      console.log("密码"+this.ruleForm.region)
+      console.log("修改密码"+this.ruleForm.type)
+      console.log("提交成功")
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
   },
 };
 </script>
@@ -61,10 +78,10 @@ export default {
   height: 170px;
   border-radius: 50%;
   float: left;
-  margin-left: 8%;
+  margin-left: 6%;
 }
-.left{
+.left {
   float: left;
-  margin-left: 50px;
+  margin-left: 70px;
 }
 </style>
